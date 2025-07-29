b78162cfafdc664c20df42791f3b7ce4938eabe6
vaibhav709 <crvaibhav191@gmail.com>
2025-07-29 11:28:21 +0000

    callbacks added

--- src/CNN_classifier/entity/config_entity.py
+++ src/CNN_classifier/entity/config_entity.py
@@ -21,4 +21,12 @@ class PrepareBaseModelConfig:
     params_weights: str
     params_classes: int
 
+@dataclass(frozen=True)
+class PrepareCallbacksConfig:
+    root_dir: Path
+    tensorboard_root_log_dir: Path
+    checkpoint_model_filepath: Path
+
+
+
 
