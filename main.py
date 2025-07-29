d01e8e18bb5a7e859e0bbb0474415afd9d275605
vaibhav709 <crvaibhav191@gmail.com>
2025-07-29 11:12:52 +0000

    preparing base model completed

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from CNN_classifier  import logger
+from CNN_classifier import logger
 from CNN_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from CNN_classifier.pipeline._02_stage_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Prepare Base Model"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
--- src/CNN_classifier/config/configuration.py
+++ src/CNN_classifier/config/configuration.py
@@ -1,6 +1,11 @@
 from CNN_classifier.constants import *
+import os
+from pathlib import Path
 from CNN_classifier.utils.common import read_yaml, create_directories
-from CNN_classifier.entity.config_entity import DataIngestionConfig
+from CNN_classifier.entity.config_entity import (DataIngestionConfig,
+                                                PrepareBaseModelConfig)
+
+
 
 class ConfigurationManager:
     def __init__(
@@ -28,4 +33,27 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+
+
+    
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        
+        create_directories([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+
+        return prepare_base_model_config
+    
+
       
